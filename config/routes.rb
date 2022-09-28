6935edcd064e3f1d2b2fa050d02c28e40fbd3193
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-28 16:31:07 +0300

    commit tree f5338ff09415b01b33c61da88d3d1e05fdc47aa9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'students', to: 'students#index'
-  get 'students/grades', to: 'students#grades'
+  get 'students', to: 'students#index'  #This is a route to display all the students.
+  get 'students/grades', to: 'students#grades' #A route to display all thr grades
+  get "students/highest-grade", to: 'students#highest_grade'  #route for finding the highest grade
+
 
 end
