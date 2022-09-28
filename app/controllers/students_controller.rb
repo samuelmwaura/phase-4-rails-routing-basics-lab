78f1fc918d1aa83fdefe594b76e52f7bedba681b
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-28 16:31:24 +0300

    completed the bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,11 @@ class StudentsController < ApplicationController
     end
 
     def grades
-        render json: Student.order(grade: :desc)
+        render json: Student.order(grade: :desc)  #students ordered in order of their grades
+    end
+
+    def highest_grade  #display the highest grade
+        render json: Student.find_by(grade: Student.maximum(:grade))  #first gets the value of the highest grade and then uses the find by method to get the one student with the value in their grade attribute
     end
 
 end
